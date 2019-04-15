b3a4078770eca19574d08a590227c66abf849fe2
Matthew Wilkens <mwilkens@nd.edu>
2019-04-15 10:14:41 -0400

    PS09 graph analysis

--- libraries/TMN.py
+++ libraries/TMN.py
@@ -476,77 +476,6 @@ class Preprocessor(object):
             self.process(fileid, chunksize=chunksize, norm=norm)
             for fileid in self.fileids(fileids, categories)
         ]
-    
-
-class KeyphraseExtractor(BaseEstimator, TransformerMixin):
-    """
-    Wraps a PickledCorpusReader consisting of pos-tagged documents.
-    """
-    def __init__(self, grammar=GRAMMAR):
-        self.grammar = GRAMMAR
-        self.chunker = RegexpParser(self.grammar)
-
-    def normalize(self, sent):
-        """
-        Removes punctuation from a tokenized/tagged sentence and
-        lowercases words.
-        """
-        is_punct = lambda word: all(unicat(char).startswith('P') for char in word)
-        sent = filter(lambda t: not is_punct(t[0]), sent)
-        sent = map(lambda t: (t[0].lower(), t[1]), sent)
-        return list(sent)
-
-    def extract_keyphrases(self, document):
-        """
-        For a document, parse sentences using our chunker created by
-        our grammar, converting the parse tree into a tagged sequence.
-        Yields extracted phrases.
-        """
-        for sents in document:
-            for sent in sents:
-                sent = self.normalize(sent)
-                if not sent: continue
-                chunks = tree2conlltags(self.chunker.parse(sent))
-                phrases = [
-                    " ".join(word for word, pos, chunk in group).lower()
-                    for key, group in groupby(
-                        chunks, lambda term: term[-1] != 'O'
-                    ) if key
-                ]
-                for phrase in phrases:
-                    yield phrase
-
-    def fit(self, documents, y=None):
-        return self
-
-    def transform(self, documents):
-        for document in documents:
-            yield list(self.extract_keyphrases(document))
-            
-            
-class EntityExtractor(BaseEstimator, TransformerMixin):
-    def __init__(self, labels=GOODLABELS, **kwargs):
-        self.labels = labels
-
-    def get_entities(self, document):
-        entities = []
-        for paragraph in document:
-            for sentence in paragraph:
-                trees = ne_chunk(sentence)
-                for tree in trees:
-                    if hasattr(tree, 'label'):
-                        if tree.label() in self.labels:
-                            entities.append(
-                                ' '.join([child[0].lower() for child in tree])
-                                )
-        return entities
-
-    def fit(self, documents, labels=None):
-        return self
-
-    def transform(self, documents):
-        for document in documents:
-            yield self.get_entities(document)
             
             
 class CorpusLoader(object):
@@ -580,3 +509,4 @@ class CorpusLoader(object):
             y_test = self.labels(test_index)
 
             yield X_train, X_test, y_train, y_test
+            
